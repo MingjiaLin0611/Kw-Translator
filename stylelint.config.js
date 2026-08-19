130021aa4f6ca82dba796c291281c293ffb0bfc7
MingjiaLin0611 <mjl980611@163.com>
2026-08-19 23:45:08 +0800

    chore: add stylelint checks

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ["stylelint-config-standard"],
+  ignoreFiles: [
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/coverage/**",
+    "**/playwright-report/**",
+    "**/test-results/**",
+  ],
+};
